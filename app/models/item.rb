9b7ea9ff9fb302397ba63498f55ff14dc6d676ec
Yutaka8842 <inoue.8842@gmail.com>
2021-04-21 16:27:39 +0900

    アクティブハッシュの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,22 @@
 class Item < ApplicationRecord
+  
   with_options presence: true do
     validates :item_name
     validates :item_description
-    validates :item_category_id
-    validates :item_status_id
-    validates :delivery_charge_id
-    validates :prefectures_id
-    validates :shipping_days_id
+    validates :item_category_id,   numericality: { other_than: 1 } 
+    validates :item_status_id,     numericality: { other_than: 1 } 
+    validates :delivery_charge_id, numericality: { other_than: 1 } 
+    validates :prefectures_id,     numericality: { other_than: 1 } 
+    validates :shipping_days_id,   numericality: { other_than: 1 } 
     validates :selling_price
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :delivery_charge
+  belongs_to :prefectures
+  belongs_to :shipping_days
+
   belongs_to :user
 end
